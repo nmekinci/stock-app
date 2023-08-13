601c2d06bbf81f8977af02ca7b1c08c08e516847
Must <emailme>
2023-08-13 17:19:55 +0200

    Firms

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,5 +1,40 @@
+import { useEffect } from "react"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import Grid from "@mui/material/Grid"
+import { useSelector } from "react-redux"
+import useStockCall from "../hooks/useStockCall"
+import FirmCard from "../components/FirmCard"
+
+
 const Firms = () => {
-  return <div>Firms</div>
+  
+
+  const { getStockData } = useStockCall()
+  const { firms } = useSelector((state) => state.stock)
+
+  useEffect(() => {
+    // getFirms()
+    getStockData("firms")
+  }, [])
+
+  console.log(firms)
+  return (
+    <div>
+      <Typography variant="h4" color={"error"} mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">NEW FIRM</Button>
+
+      <Grid container justifyContent={"center"} spacing={2}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  )
 }
 
 export default Firms
