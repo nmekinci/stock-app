8070f925ff0447150207ed9cad054ce8f256bd9d
Must <emailme>
2023-08-17 16:33:37 +0200

    product.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,37 @@
+import { useSelector } from "react-redux"
+import useStockCall from "../hooks/useStockCall"
+import  Typography  from "@mui/material/Typography"
+import Button  from "@mui/material/Button"
 
 const Products = () => {
+
+  const {getStockData} = useStockCall()
+  const {products} = useSelector( (state) => state.stock)
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+
+
+  useEffect(() => {
+   getStockData("products")
+   getStockData("categories")
+   getStockData("brands")
+  }, [])
+  
   return (
-    <div>Products</div>
+    <div>
+      <Typography variant="h4" color={"error"} mb={3}>
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        NEW PRODUCT
+      </Button>
+
+      <ProductModal open={open} handleClose={handleClose} />
+      <ProductTable />
+    </div>
   )
 }
 
