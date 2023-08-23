f2e52d9d62387b61f193a121a2ad28fb24489323
Must <emailme>
2023-08-23 13:07:20 +0200

    update: sale.jsx

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,17 +1,63 @@
-import { useEffect } from "react"
-import useStockCall from "../hooks/useStockCall"
-import { useSelector } from "react-redux"
+import { useEffect } from "react";
+import useStockCall from "../hooks/useStockCall";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { SettingsApplications } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
 
 const Sales = () => {
-  const { getStockData } = useStockCall()
-  const { sales } = useSelector((state) => state.stock)
+  const { getStockData, getProdCatBrands } = useStockCall();
+  // const { sales } = useSelector((state) => state.stock)
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+
+  const [info, setInfo] = useState({
+    brand_id: "",
+    product_id: "",
+    quantity: "",
+    price: "",
+  });
+
+  const handleClose = () => {
+    setOpen(false);
+    setInfo({
+      brand_id: "",
+      product_id: "",
+      quantity: "",
+      price: "",
+    });
+  };
 
   useEffect(() => {
-    getStockData("sales")
-  }, [])
+    getProdCatBrands()
+    getStockData("sales");
+  }, []); // eslint-disable-line
 
-  console.log(sales)
-  return <div>Sales</div>
-}
+  // console.log(sales);
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Sales
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ marginBottom: "1rem" }}
+        onClick={() => setOpen(true)}
+      >
+        New Sale
+      </Button>
 
-export default Sales
+      <SaleModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+
+      <SaleTable handleOpen={handleOpen} setInfo={setInfo} />
+    </div>
+  );
+};
+
+export default Sales;
