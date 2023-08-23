48f4e3f22ab37929747a05b553904f4565742c94
Must <emailme>
2023-08-23 13:58:43 +0200

    purchases.jsx

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,19 +1,53 @@
-import { useSelector } from "react-redux";
-import useStockCall from "../hooks/useStockCall";
-import { useEffect } from "react";
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { useEffect, useState } from "react"
+import useStockCall from "../hooks/useStockCall"
+import PurchaseModal from "../components/PurchaseModal"
+import PurchaseTable from "../components/PurchaseTable"
 
 const Purchases = () => {
+  const { getStockData, getProdCatBrands } = useStockCall()
+  const [open, setOpen] = useState(false)
 
-  const {getStockData} = useStockCall()
-  const { purchases} = useSelector( (state) => state.stock)
+  const [info, setInfo] = useState({
+    brand_id: "",
+    product_id: "",
+    quantity: "",
+    price: "",
+  })
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setInfo({ brand_id: "", product_id: "", quantity: "", price: "" })
+  }
 
   useEffect(() => {
+    getProdCatBrands()
     getStockData("purchases")
-  
-   
-  }, [])
-  console.log(purchases);
-  return <div>Purchases</div>;
-};
+    getStockData("firms")
+  }, []) // eslint-disable-line
 
-export default Purchases;
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Purchases
+      </Typography>
+
+      <Button variant="contained" onClick={handleOpen}>
+        New Purchase
+      </Button>
+
+      <PurchaseModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+
+      <PurchaseTable handleOpen={handleOpen} setInfo={setInfo} />
+    </div>
+  )
+}
+
+export default Purchases
