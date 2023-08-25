32b0d8080c2bf012b2485652bdbcac6a89c8afcf
Must <nm.ekinci@gmail.com>
2023-08-25 16:12:26 +0200

    noauthpage.jsx

--- src/components/NoAuthPage.jsx
+++ src/components/NoAuthPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import {styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(() => ({
+  textAlign: "center",
+  color: "tomato",
+  height: 60,
+  lineHeight: "60px",
+}));
 
 const NoAuthPage = () => {
   return (
-    <div>NoAuthPage</div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        "& > :not(style)": {
+          m: 1,
+          width: 228,
+          height: 60,
+        },
+      }}
+    >
+      <Item elevation={3} sx={{ alignItems: "center" }}>
+        You are not AUTHORIZED
+      </Item>
+    </Box>
+  );
+};
 
-export default NoAuthPage
+export default NoAuthPage;
