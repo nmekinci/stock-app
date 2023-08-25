e703e9d04d33613319c3e24f2564079aca560ece
Must <nm.ekinci@gmail.com>
2023-08-25 16:34:53 +0200

    comment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "./build" },
+  build: { outDir: "./build" },//with this line vite builds it as a build folder not as a dist folder. for easy deploy on netlify or github
 })
